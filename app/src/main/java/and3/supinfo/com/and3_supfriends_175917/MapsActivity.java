7a8083401e855478f662fae3d5062e9d9aab84ca
yiao <yiao.sun@supinfo.com>
2016-05-26 10:56:43 +0200

    Ajoute position GPS

--- app/src/main/java/and3/supinfo/com/and3_supfriends_175917/MapsActivity.java
+++ app/src/main/java/and3/supinfo/com/and3_supfriends_175917/MapsActivity.java
@@ -1,6 +1,10 @@
 package and3.supinfo.com.and3_supfriends_175917;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -69,34 +73,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .position(mypostion));
 
 
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            return;
-//        }
-//        mMap.setMyLocationEnabled(true);
-//        // Check if we were successful in obtaining the map.
-//        if (mMap != null) {
-//
-//
-//            mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-//
-//                @Override
-//                public void onMyLocationChange(Location arg0) {
-//                    // TODO Auto-generated method stub
-//
-//                    LatLng myLat = new LatLng(arg0.getLatitude(), arg0.getLongitude());
-//
-//                    mMap.addMarker(new MarkerOptions()
-//                            .title("My Position")
-//                            .position(myLat)
-//                            .snippet("The most populous city in Australia."));
-//                }
-//            });
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        mMap.setMyLocationEnabled(true);
+        // Check if we were successful in obtaining the map.
+        if (mMap != null) {
+
+
+            mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+
+                @Override
+                public void onMyLocationChange(Location arg0) {
+                    // TODO Auto-generated method stub
+
+                    LatLng myLat = new LatLng(arg0.getLatitude(), arg0.getLongitude());
+
+                    mMap.addMarker(new MarkerOptions()
+                            .title("Your Position")
+                            .position(myLat)
+                            .snippet("Here I am"));
+                }
+            });
+        }
+    }
 }
